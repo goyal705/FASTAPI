8230b7346ec0bec77f3423d7ada842a9bafbb397
Tushar Goyal <tushargoyal@primuspartners.in>
2025-05-27 19:28:41 +0530

    fe:added requiremnets.txt

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get('/')
 def getmethod():
     return {"status":True,"message":"Hey"}
+
+#this would throw error if is present before /blog/{blog_id} as pydantic would try to validate
+#unpublished as int
+@app.get('/blog/unpublished')
+def blog():
+    return {"status":True,"message":34}
+
+#to accept query parameter
+@app.get('/blog/publish')
+def printname(name):
+    return {"status":True,"message":f"Name of the publisher is {name}"}
+
+@app.get('/blog/{blog_id}')
+def blog(blog_id:int):
+    return {"status":True,"message":blog_id}
